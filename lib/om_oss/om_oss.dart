e0f896f2f9eecfc203a5dbd2479a11075f3112d3
Monica <42612669+MonicaFoss@users.noreply.github.com>
2020-05-19 14:52:48 +0200

    Lagt til padding, fontstørrelse etc

--- lib/om_oss/om_oss.dart
+++ lib/om_oss/om_oss.dart
@@ -6,9 +6,19 @@ class OmOss extends StatelessWidget {
   Widget build(BuildContext context) {
     return Layout(
       appBarText: 'Min Reise',
-      customBody: Container(
-        child: Text('Om oss'),
-      ),
+      customBody: Column(children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: Text(
+            'Om oss',
+            style: TextStyle(
+              color: Color(0xff383E42),
+              fontFamily: 'Raleway', // Does not exist in this project
+              fontSize: 30,
+            ),
+          ),
+        ),
+      ]),
     );
   }
 }
