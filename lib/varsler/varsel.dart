c66dee0022e51e97ec30ae5077b9149fbf864186
Sindre Johan Vinje <Sindrjv@macbook-pro-4.lan>
2020-05-22 11:59:37 +0200

    add varsler-class

---
+++ lib/varsler/varsel.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Varsel {
+  String title;
+  String subtitle;
+  TimeOfDay timeStamp;
+  Icon catagoryIcon;
+  Icon isDelayed;
+  Color catagoryColor;
+
+  bool userHasRead = false;
+
+  Varsel({
+    this.title,
+    this.subtitle,
+    this.timeStamp,
+    this.catagoryIcon,
+    this.catagoryColor,
+    this.isDelayed,
+    this.userHasRead
+  });
+  void setUserHasRead(){
+    userHasRead = true;
+  }
+}
