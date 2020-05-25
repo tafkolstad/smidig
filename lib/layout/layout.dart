32df160e7c7d8d18c704076a9c6bd4b0e4264a13
alfmagnus <alf-magnus@hotmail.com>
2020-05-25 07:02:40 +0200

    diverse design fiks

--- lib/layout/layout.dart
+++ lib/layout/layout.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'bars/custom_bottom_bar.dart';
 import 'bars/custom_app_bar.dart';
+import 'package:vy_test/layout/colors.dart';
 
 class Layout extends Scaffold {
   final String appBarText;
@@ -9,7 +10,7 @@ class Layout extends Scaffold {
 
   Layout({this.appBarText, this.appBarButtons = true, this.customBody})
       : super(
-          backgroundColor: Color(0xFFEDEAEA),
+          backgroundColor: backgroudColor,
           appBar: CustomAppBar(
             titleText: appBarText,
             showButtons: appBarButtons,
