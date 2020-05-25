7c23cc48ae64efb0bfcd8c830220d2b39ccb44be
alfmagnus <alf-magnus@hotmail.com>
2020-05-25 07:02:40 +0200

    diverse design fiks

--- lib/meny/meny_item.dart
+++ lib/meny/meny_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vy_test/layout/colors.dart';
 
 class MenyItem extends StatelessWidget {
   final String menuItemText;
@@ -16,21 +17,31 @@ class MenyItem extends StatelessWidget {
         decoration: BoxDecoration(
           boxShadow: [
             BoxShadow(
-              color: Colors.grey,
-              offset: Offset(3, 3),
-              blurRadius: 3,
+              color: dropshadowColor,
+              offset: Offset(1, 4),
+              blurRadius: 6,
             ),
           ],
-          borderRadius: BorderRadius.circular(15.0),
+          borderRadius: BorderRadius.circular(7.0),
           color: Colors.white,
         ),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.end,
           children: <Widget>[
             Image(
               image: AssetImage("assets/$menuItemImage"),
             ),
-            Text(menuItemText),
+            Padding(
+              padding: EdgeInsets.only(top: 10.0, bottom: 12.0),
+              child: Text(
+                menuItemText,
+                style: TextStyle(
+                  fontFamily: 'Raleway',
+                  fontSize: 20.0,
+                  color: textColorMenu,
+                ),
+              ),
+            ),
           ],
         ),
       ),
