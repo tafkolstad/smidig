7215aec293acce9e946c0a63bb0fa95228e1be71
alf magnus lie <alf-mangus@hotmail.com>
2020-05-26 15:58:30 +0200

    fix

---
+++ lib/severdigheter/Widgets/topPlaces_karusell.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+import 'package:vy_test/severdigheter/Model/topPlaces_model.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:vy_test/layout/colors.dart';
+
+class DestinationCarousel extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                'Top Destinations',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: textColorMenu,
+                  fontFamily: 'Segoe UI',
+                  letterSpacing: 0.3,
+                ),
+              ),
+              GestureDetector(
+                onTap: () => print('view all'),
+                child: Text(
+                  'View all',
+                  style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontSize: 13.0,
+                    fontFamily: 'Segoe UI',
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: 1.0,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          height: 300.0,
+          padding: EdgeInsets.symmetric(horizontal: 15.0),
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: destinations.length,
+            itemBuilder: (BuildContext context, int index) {
+              Destination destination = destinations[index];
+              return GestureDetector(
+                child: Container(
+                  margin: EdgeInsets.all(10.0),
+                  width: 250.0,
+                  child: Stack(
+                    alignment: Alignment.topCenter,
+                    children: <Widget>[
+                      Container(
+                        child: Stack(
+                          children: <Widget>[
+                            Hero(
+                              tag: destination.imageUrl,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(20.0),
+                                child: Image(
+                                  height: 180.0,
+                                  width: 260.0,
+                                  image: AssetImage(destination.imageUrl),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              left: 15.0,
+                              bottom: 30.0,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Text(
+                                    destination.place,
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 24.0,
+                                      fontWeight: FontWeight.w600,
+                                      letterSpacing: 1.2,
+                                    ),
+                                  ),
+                                  Row(
+                                    children: <Widget>[
+                                      Icon(
+                                        FontAwesomeIcons.locationArrow,
+                                        size: 10.0,
+                                        color: Colors.white,
+                                      ),
+                                      SizedBox(width: 8.0),
+                                      Text(
+                                        destination.place,
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 25.0,
+                        child: Container(
+                          height: 100.0,
+                          width: 230.0,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.fromLTRB(15, 5, 10, 5),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  destination.place,
+                                  style: TextStyle(
+                                    fontSize: 22.0,
+                                    fontFamily: 'Segoe UI',
+                                    letterSpacing: 0.2,
+                                  ),
+                                ),
+                                SizedBox(height: 3.0,),
+                                Text(
+                                  destination.description,
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 3,
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/severdigheter/severdigheter.dart
+++ lib/severdigheter/severdigheter.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:vy_test/layout/layout.dart';
-import 'severdighet.dart';
-import 'severdighetliten.dart';
+import 'package:vy_test/severdigheter/Widgets/topPlaces_karusell.dart';
+
 
 class Severdigheter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Layout(
       appBarText: 'Min Reise',
-      customBody: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      customBody: ListView(
         children: <Widget>[
           Padding(
-            padding: const EdgeInsets.fromLTRB(30, 30, 0, 20),
+            padding: const EdgeInsets.fromLTRB(30, 30, 0, 0),
             child: Text(
               'Severdigheter',
               style: TextStyle(
@@ -22,100 +21,9 @@ class Severdigheter extends StatelessWidget {
                   fontSize: 30),
             ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.fromLTRB(35, 0, 10, 10),
-                child: Text('Suggested top locations',
-                    style: TextStyle(
-                        fontFamily: 'Segoe',
-                        fontWeight: FontWeight.w600,
-                        color: Color(0xff7C847C),
-                        fontSize: 13.5)),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 0, 15, 10),
-                child: InkWell(
-                  child: Text(
-                    'View all > ',
-                    style: TextStyle(
-                        fontFamily: 'Segoe',
-                        fontWeight: FontWeight.w600,
-                        color: Color(0xff7C847C),
-                        fontSize: 13.5),
-                  ),
-                  onTap: () {
-                    Navigator.pushNamed(context, '/severdigheterviewall');
-                  },
-                ),
-              ),
-            ],
-          ),
-          new Expanded(
-              child: ListView(
-              scrollDirection: Axis.horizontal,
-              children: <Widget>[
-                Severdighet(
-                  severdighetBilde: 'stranda.png',
-                  severdighetOverskrift: 'Stranda',
-                  severdighetTekst: 'Dette er ei bygd. Det er faktisk den flottaste bygda i helie verden. Dei lagar grandiosa her nemlig.',
-                ),
-                Severdighet(
-                  severdighetBilde: 'fjord2.png',
-                  severdighetOverskrift: 'Fjorden',
-                  severdighetTekst: 'Fjord er en landform som i internasjonalt fagspråk (geologi, geografi) forklart som ei dyp, smal',
-                ),
-                Severdighet(
-                  severdighetBilde: 'by.png',
-                  severdighetOverskrift: 'By',
-                  severdighetTekst: 'Dette er ein by',
-                ),
-              ],
-            )
-          ),
-
-          Container(
-            margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
-            padding: const EdgeInsets.fromLTRB(30, 0, 0, 0),
-            child: Text('Neste oppkommende',
-                style: TextStyle(
-                    fontFamily: 'Segoe',
-                    color: Color(0xff383E42),
-                    fontWeight: FontWeight.w600,
-                    fontSize: 25)),
-          ),
-          
-          new Expanded(
-              child: ListView(
-            scrollDirection: Axis.horizontal,
-            children: <Widget>[
-              SeverdighetLiten(
-                severdighetLitenBilde: 'fjordliten.png',
-                severdighetLitenOverskrift: 'Trolltunga',
-                severdighetLitenTid: '6 min',
-                severdighetLitenSide: 'Venstre side',
-              ),
-              SeverdighetLiten(
-                severdighetLitenBilde: 'fjordliten2.png',
-                severdighetLitenOverskrift: 'Vekje karr',
-                severdighetLitenTid: '23 min',
-                severdighetLitenSide: 'Høyre side',
-              ),
-              SeverdighetLiten(
-                severdighetLitenBilde: 'fjordliten3.png',
-                severdighetLitenOverskrift: 'Ulriken',
-                severdighetLitenTid: '34 min',
-                severdighetLitenSide: 'Høyre side',
-              ),
-              SeverdighetLiten(
-                severdighetLitenBilde: 'fjordliten2.png',
-                severdighetLitenOverskrift: 'Vekje karr',
-                severdighetLitenTid: '48 min',
-                severdighetLitenSide: 'Venstre side',
-              ),
-            ],
-          )),
+          SizedBox(height: 20.0),
+          DestinationCarousel(),
+      
         ],
       ),
     );
