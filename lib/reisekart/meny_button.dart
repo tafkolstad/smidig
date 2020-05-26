69ff42f8093f5c42dfcfe40bb307ff83a091627e
Tony André Furuhaug Kolstad <kolton18@EGMS.no>
2020-05-26 13:48:46 +0200

    Make billetter from navbar and reisekart from menu remove everything from stack, make MenyItem more dynamic in size, split menu button and sidebar in separate files

---
+++ lib/reisekart/meny_button.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:vy_test/layout/colors.dart';
+
+class MenuButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final _edgePadding = MediaQuery.of(context).size.height * 0.01;
+    final _buttonPaddingInside = MediaQuery.of(context).size.width * 0.1;
+    final _buttonHeight = MediaQuery.of(context).size.height * 0.05;
+
+    return Container(
+      alignment: Alignment.bottomCenter,
+      child: Padding(
+        padding: EdgeInsets.only(
+          bottom: _edgePadding,
+        ),
+        child: ButtonTheme(
+          padding: EdgeInsets.symmetric(
+            horizontal: _buttonPaddingInside,
+          ),
+          height: _buttonHeight,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+          child: Builder(
+            builder: (context) {
+              return RaisedButton(
+                color: Colors.white,
+                child: Text(
+                  'Meny',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: 'Segoe UI',
+                    fontSize: 21.0,
+                    fontWeight: FontWeight.w600,
+                    color: vyColorDarkGreen,
+                  ),
+                ),
+                onPressed: () {
+                  Scaffold.of(context).openEndDrawer();
+                },
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/reisekart/reisekart.dart
+++ lib/reisekart/reisekart.dart
@@ -1,117 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:vy_test/layout/colors.dart';
 import 'package:vy_test/layout/layout.dart';
-import 'package:vy_test/meny/meny_item.dart';
+import 'package:vy_test/reisekart/meny_button.dart';
+import 'package:vy_test/reisekart/sidebar.dart';
 
 class Reisekart extends StatelessWidget {
-  static const TextStyle _textStyle = TextStyle(
-    fontFamily: 'Raleway',
-    fontSize: 12.0,
-    color: textColorMenu,
-  );
-
   @override
   Widget build(BuildContext context) {
-  final _edgePadding = MediaQuery.of(context).size.width * 0.03;
-  final _menuButtonPadding = MediaQuery.of(context).size.width * 0.1;
-  final _menuButtonHeight = MediaQuery.of(context).size.height * 0.05;
 
     return Layout(
       appBarText: 'Min Reise',
-
-      // Sidebar
-      customDrawer: Container(
-        width: 120.0,
-        color: Colors.transparent,
-        child: ListView(
-          padding: EdgeInsets.only(
-            right: _edgePadding,
-          ),
-          shrinkWrap: true,
-          children: <Widget>[
-            MenyItem(
-              menuItemImage: 'mat.png',
-              menuItemText: 'Mat',
-              routeName: 'mat',
-              textStyle: _textStyle,
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            MenyItem(
-              menuItemImage: 'vognoversikt.png',
-              menuItemText: 'Vognoversikt',
-              routeName: 'vognoversikt',
-              textStyle: _textStyle,
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            MenyItem(
-              menuItemImage: 'severdigheter.png',
-              menuItemText: 'Severdigheter',
-              routeName: 'severdigheter',
-              textStyle: _textStyle,
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            MenyItem(
-              menuItemImage: 'betjening.png',
-              menuItemText: 'Betjening',
-              routeName: 'betjening',
-              textStyle: _textStyle,
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            MenyItem(
-              menuItemImage: 'mer_info.png',
-              menuItemText: 'Mer info',
-              routeName: 'meny',
-              textStyle: _textStyle,
-            ),
-          ],
-        ),
-      ),
-
+      customDrawer: Sidebar(),
       // Menu button
-      customBody: Container(
-        alignment: Alignment.bottomCenter,
-        child: Padding(
-          padding: EdgeInsets.only(
-            bottom: _edgePadding,
-          ),
-          child: ButtonTheme(
-            padding: EdgeInsets.symmetric(
-              horizontal: _menuButtonPadding,
-            ),
-            height: _menuButtonHeight,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15.0)),
-            child: Builder(
-              builder: (context) {
-                return RaisedButton(
-                  color: Colors.white,
-                  child: Text(
-                    'Meny',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontFamily: 'Segoe UI',
-                      fontSize: 21.0,
-                      fontWeight: FontWeight.w600,
-                      color: vyColorDarkGreen,
-                    ),
-                  ),
-                  onPressed: () {
-                    Scaffold.of(context).openEndDrawer();
-                  },
-                );
-              },
-            ),
-          ),
-        ),
-      ),
+      customBody: MenuButton(),
     );
   }
 }
