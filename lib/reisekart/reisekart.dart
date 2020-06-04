0ff27cc32ffd6317a58802586588bce3ad14900d
alf magnus lie <alf-mangus@hotmail.com>
2020-06-04 08:52:49 +0200

    topbar button and popstack fix

--- lib/reisekart/reisekart.dart
+++ lib/reisekart/reisekart.dart
@@ -36,36 +36,42 @@ class _ReisekartState extends State<Reisekart> {
 
     getPermission();
 
-    return Layout(
-      appBarText: 'Min Reise',
-      appBarButtons: false,
-      customDrawer: Sidebar(),
-      customBody: Stack(
-        children: <Widget>[
-          Column(
-            children: <Widget>[
-              Expanded(
-                child: MapboxMap(
-                  myLocationEnabled: true,
-                  styleString:
-                      'mapbox://styles/sindrejv/ckawdi85r0f6v1impfct49vq2',
-                  onMapCreated: androidController,
-                  trackCameraPosition: true,
-                  initialCameraPosition: CameraPosition(
-                      target: LatLng(59.9139, 10.7522), zoom: 13.0, tilt: 50),
+    return WillPopScope(
+          onWillPop: () async{
+            Navigator.of(context).pushNamedAndRemoveUntil('/', (Route<dynamic> route) => false);
+            return true;          
+          },
+          child: Layout(
+        appBarText: 'Min Reise',
+
+        customDrawer: Sidebar(),
+        customBody: Stack(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Expanded(
+                  child: MapboxMap(
+                    myLocationEnabled: true,
+                    styleString:
+                        'mapbox://styles/sindrejv/ckawdi85r0f6v1impfct49vq2',
+                    onMapCreated: androidController,
+                    trackCameraPosition: true,
+                    initialCameraPosition: CameraPosition(
+                        target: LatLng(59.9139, 10.7522), zoom: 13.0, tilt: 50),
+                  ),
                 ),
-              ),
-            ],
-          ),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Expanded(child: StoppestedButton(),),
-              Expanded(child: VarselButton()),
-            ],
-          ),
-          MenuButton(),
-        ],
+              ],
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Expanded(child: StoppestedButton(),),
+                Expanded(child: VarselButton()),
+              ],
+            ),
+            MenuButton(),
+          ],
+        ),
       ),
     );
   }
