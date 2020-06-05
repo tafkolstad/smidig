b5b4dccd3478d6ef1371508675628b229f073749
Sindre Johan Vinje <Sindrjv@macbook-pro-4.lan>
2020-06-05 09:09:16 +0200

    merge

--- lib/reisekart/reisekart.dart
+++ lib/reisekart/reisekart.dart
@@ -39,25 +39,6 @@ class _ReisekartState extends State<Reisekart> {
 
     getPermission();
 
-<<<<<<< HEAD
-    return Layout(
-      appBarText: 'Min Reise',
-      appBarButtons: false,
-      customDrawer: Sidebar(),
-      customBody: Stack(
-        children: <Widget>[
-          Column(
-            children: <Widget>[
-              Expanded(
-                child: MapboxMap(
-                  // myLocationEnabled: true,
-                  styleString:
-                      'mapbox://styles/sindrejv/ckawdi85r0f6v1impfct49vq2',
-                  onMapCreated: androidController,
-                  trackCameraPosition: true,
-                  initialCameraPosition: CameraPosition(
-                      target: LatLng(59.9139, 10.7522), zoom: 13.0, tilt: 50),
-=======
     return WillPopScope(
           onWillPop: () async{
             Navigator.of(context).pushNamedAndRemoveUntil('/', (Route<dynamic> route) => false);
@@ -82,7 +63,6 @@ class _ReisekartState extends State<Reisekart> {
                     initialCameraPosition: CameraPosition(
                         target: LatLng(59.9139, 10.7522), zoom: 13.0, tilt: 50),
                   ),
->>>>>>> 5d0fa178937fd030e3ce65b6824f1f358ed4c55b
                 ),
               ],
             ),
